cad2f3b6d3eb0a6f1582d3e3df0087737560ed85
YangShuai <shuai.yang@palmaplus.com>
2017-04-18 14:09:05 +0800

    edit distance

---
+++ src/main/scala/space/yangshuai/nodsolution/tutorial/greedy/EditDistance.scala
@@ -0,0 +1,56 @@
+package space.yangshuai.nodsolution.tutorial.greedy
+
+/**
+  * Created on 2017/4/18.
+  *
+  * @author shuai.yang
+  */
+object EditDistance {
+
+  def main(args: Array[String]): Unit = {
+    val a = scala.io.StdIn.readLine().toCharArray
+    val b = scala.io.StdIn.readLine().toCharArray
+
+    val upper = new Array[Int](b.length + 1)
+    val current = new Array[Int](b.length + 1)
+
+    for (y <- 0 to a.length) {
+      System.arraycopy(current, 0, upper, 0, b.length + 1)
+      for (x <- 0 to b.length) {
+        if (x == 0) {
+          current(x) = y
+        } else if (y == 0) {
+          current(x) = x
+        } else {
+          current(x) = min(upper(x - 1) + same(a(y - 1), b(x - 1)), upper(x) + 1, current(x - 1) + 1)
+        }
+      }
+    }
+
+    println(current(b.length))
+  }
+
+  def same(a: Char, b: Char): Int = {
+    if (a == b) {
+      0
+    } else {
+      1
+    }
+  }
+
+  def min(a: Int, b: Int, c: Int): Int = {
+    if (a > b) {
+      if (b < c) {
+        b
+      } else {
+        c
+      }
+    } else {
+      if (a < c) {
+        a
+      } else {
+        c
+      }
+    }
+  }
+}
