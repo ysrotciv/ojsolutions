e47384dc69a33f214eabadba25f4b449e5bb852b
YangShuai <frank.yang@kuaigeng.com>
2019-04-03 23:09:14 +0800

    add array related

--- src/main/java/space/yangshuai/ojsolutions/leetcode/lessons/array/MoveZero.java
+++ src/main/java/space/yangshuai/ojsolutions/leetcode/lessons/array/MoveZero.java
@@ -1,5 +1,7 @@
 package space.yangshuai.ojsolutions.leetcode.lessons.array;
 
+import java.util.Arrays;
+
 /**
  * @author yangshuai on 2019-04-03.
  */
@@ -9,12 +11,22 @@ public class MoveZero {
 
         int index = 0;
         for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] != 0 && i != index) {
-                nums[index++] = nums[i];
-                nums[i] = 0;
+            if (nums[i] != 0) {
+                if (i != index) {
+                    nums[index++] = nums[i];
+                    nums[i] = 0;
+                } else {
+                    index++;
+                }
             }
         }
 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,0,3,12};
+        new MoveZero().moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
 }
