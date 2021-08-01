c43d31f582bd00e4e07ef2cf631a05023febaad2
rotciv <ysrotciv@gmail.com>
2021-08-01 09:01:01 +0800

    contest 251

---
+++ src/main/java/space/yangshuai/ojsolutions/leetcode/contest/contest251/SolutionA.java
@@ -0,0 +1,30 @@
+package space.yangshuai.ojsolutions.leetcode.contest.contest251;
+
+public class SolutionA {
+
+    public int getLucky(String s, int k) {
+
+        StringBuilder builder = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            builder.append(c - 'a' + 1);
+        }
+        String raw = builder.toString();
+        for (int i = 0; i < k; i++) {
+            int tmp = getSum(raw);
+            raw = String.valueOf(tmp);
+        }
+        return Integer.parseInt(raw);
+    }
+
+    private int getSum(String s) {
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            res += Character.getNumericValue(c);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new SolutionA().getLucky("zbax", 2);
+    }
+}
